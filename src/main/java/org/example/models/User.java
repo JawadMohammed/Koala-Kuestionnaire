c6d72353fe31522b3e3121bc1c605431cb7e2e1c
Viraj <virajpatel3@cmail.carleton.ca>
2024-11-10 15:07:15 -0500

    Retrying a failed commit

--- src/main/java/org/example/models/User.java
+++ src/main/java/org/example/models/User.java
@@ -4,9 +4,7 @@ public abstract class User {
     private String name;
     private int id;
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
     public void setName(String name) {
         this.name = name;
