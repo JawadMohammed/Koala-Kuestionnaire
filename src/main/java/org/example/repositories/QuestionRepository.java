f9b34b4ac40e0cbecca723a574bc8a196ecb0a0b
Jawad <jayzfire01@gmail.com>
2024-11-22 15:44:08 -0500

    small incrementations on take, actually, had to make some changes to be able to write to database

--- src/main/java/org/example/repositories/QuestionRepository.java
+++ src/main/java/org/example/repositories/QuestionRepository.java
@@ -1,6 +1,9 @@
 package org.example.repositories;
 import org.example.models.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface QuestionRepository extends CrudRepository<Question, Long> {
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
 }
