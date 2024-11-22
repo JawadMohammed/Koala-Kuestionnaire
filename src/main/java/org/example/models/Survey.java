47fd4daf1ff783cfaf4a0f39135c5df64ee538de
Jawad <jayzfire01@gmail.com>
2024-11-21 20:27:31 -0500

    deleted more trash

--- src/main/java/org/example/models/Survey.java
+++ src/main/java/org/example/models/Survey.java
@@ -3,8 +3,7 @@ package org.example.models;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import jakarta.persistence.*;
 import java.util.List;
@@ -18,92 +17,20 @@ import java.util.Objects;
  */
 
 @Entity
+@Data
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
 public class Survey {
-    /* Survey title */
-    @Getter
-    @Setter
-    private String title;
 
-    /* Survey description */
-    @Getter
-    @Setter
-    private String description;
-
-
-    /* Survey ID */
-    @Getter
-    @Setter
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
-    private long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "sId")
+    private int sid;
 
+    @Column(name = "user_Id")
+    private long user_id;
 
-    /* The questions prompt*/
-//    @Getter
-//    @Setter
-//    @ManyToOne
-//    @JoinColumn(name = "answer_id")
-//    private Answer answer;
-
-    @OneToMany(mappedBy = "survey", cascade = CascadeType.ALL)
-    private List<Answer> answers;
-
-    /* Survey questions */
-    @Getter
-    @Setter
-    private LocalDateTime createdAt;
-
-    private boolean closed;
-
-    @Getter
-    @OneToMany(mappedBy = "survey", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Question> questions;
-
-    public Survey(String title, String description, List<Question> questions){
-        this.title = title;
-        this.description = description;
-        this.questions = questions;
-    }
-
-    public Survey() {
-
-    }
-
-    /**
-     * Check if Survey is equal to another Object (i.e., another Survey)
-     * Do not check if the surveyee is equal because that is not a use-case of this function (redundant to check)
-     * Do not check if the surveyor who created this Survey is the same because the Survey IDs would differ (redundant to check)
-     * Assumption: Two different surveyors cannot create a survey with the same ID
-     *
-     * @param o - the object we are checking for equality
-     * @return boolean - True if equals; False otherwise
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        // Cast the Object o to Survey type
-        Survey survey = (Survey) o;
-
-        return Objects.equals(id, survey.id) &&
-                Objects.equals(title, survey.title) &&
-                Objects.equals(description, survey.description) &&
-                Objects.equals(questions, survey.questions);
-    }
-
-    /**
-     * Method to hash the Survey
-     * Assumption: Need not hash the surveyee because they may vary over time, resulting in different hashes
-     *
-     * @return int - the hash of our survey
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, description, questions);
-    }
-
-    public void closeSurvey() {
-        closed = true;
-    }
+    @Column(name = "closed")
+    private Boolean closed;
 }
