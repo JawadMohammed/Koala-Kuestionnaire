b61c001888ea1791df155df334b40252659180fc
Viraj <virajpatel3@cmail.carleton.ca>
2024-11-24 17:07:17 -0500

    newUser repo

---
+++ src/main/java/org/example/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package org.example.repositories;
+
+import org.example.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // Custom queries can be added here (if needed)
+}
