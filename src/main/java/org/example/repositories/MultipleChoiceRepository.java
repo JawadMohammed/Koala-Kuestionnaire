825227bac913fc06a6787dc1503a1333276a890d
aaldayel <aaldayel@hotmail.co.uk>
2024-11-25 05:35:15 -0500

    Implemented queries in answer repositories

--- src/main/java/org/example/repositories/MultipleChoiceRepository.java
+++ src/main/java/org/example/repositories/MultipleChoiceRepository.java
@@ -1,8 +1,15 @@
 package org.example.repositories;
-
 import org.example.models.MultipleChoice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.example.models.MultipleChoiceAnswer;
+import java.util.List;
 
 public interface MultipleChoiceRepository extends JpaRepository<MultipleChoice, Integer> {
-    // Add custom queries if needed
+    @Query("SELECT DISTINCT m.submission_id FROM MultipleChoiceAnswer m")
+    List<Integer> findDistinctSubmissionIds();
+
+    @Query("SELECT m FROM MultipleChoiceAnswer m WHERE m.submission_id = :submission_id")
+    List<MultipleChoiceAnswer> findBySubmissionId(Integer submission_id);
+
 }
